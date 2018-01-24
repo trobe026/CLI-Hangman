b972948ca0cfb8461b14792290b722bf91c831c9
Travis Roberts <travis.roberts2@gmail.com>
2018-01-24 02:53:09 -0600

    building game menu

--- cli.js
+++ cli.js
@@ -3,9 +3,60 @@ var Word = require('./word.js');
 
 
 
-// playGame();
-var wordLetters = new Word();
-console.log(wordLetters.splitBlanks.join(" "));
-// console.log(wordLetters.splitBlanks);
-wordLetters.playGame();
-// console.log(wordLetters.totalGuesses);
+
+var runProg = true;
+
+exports.mainMenu = function(wordLetters) {
+  this.wordLetters = new Word();
+  var that = this;
+  if (!runProg) {
+    console.log("Thanks for playing! See you next time!")
+  } else {
+    inquirer.prompt([
+      {
+        type: "list",
+        name: "choice",
+        message: "\nWelcome to Hangman - Instruments!\n Choose an option below:\n",
+        choices: [
+          'Start Game!',
+          new inquirer.Separator(),
+          'List Word Choices',
+          new inquirer.Separator(),
+          'Quit\n'
+        ]
+      }
+    ])
+    .then(function(response) {
+      if (response.choice === 'Start Game!') {
+        console.log(that.wordLetters.splitBlanks.join(" "));
+        that.wordLetters.playGame();
+      } else if (response.choice === 'List Word Choices') {
+        console.log(that.wordLetters.instruments);
+        inquirer.prompt([
+          {
+            type: "list",
+            name: "choice",
+            message: "Now What?\n",
+            choices: [
+              "Let's Play!",
+              'Back to Main Menu'
+            ]
+          }
+        ])
+        .then(function(response) {
+          if (response.choice === "Let's Play!") {
+            console.log(that.wordLetters.splitBlanks.join(" "));
+            that.wordLetters.playGame();
+          } else if (response.choice === 'Back to Main Menu') {
+            exports.mainMenu();
+          }
+        })} else if (response.choice === 'Quit\n') {
+          runProg = false;
+          exports.mainMenu();
+        }
+    });
+  }
+}
+exports.mainMenu();
+
+// module.exports = mainMenu;
--- word.js
+++ word.js
@@ -1,25 +1,36 @@
 var inquirer = require('inquirer');
 var LtrTrans = require('./letters.js');
+var mainMenu = require('./cli.js');
 
 
 var Word = function() {
+  let that = this;
+  // getting instrument
   this.instruments = ["mandolin", "harpsichord", "electricbass", "bagpipes", "electricguitar", "xylophone", "ukulele", "tambourine", "theremin", "synthesizer", "clarinet", "saxophone", "flugelhorn", "glockenspiel", "recorder"];
-  this.randomWord = this.instruments[Math.floor(Math.random() * this.instruments.length)];
-  this.word = new LtrTrans(this.randomWord);
+  this.randomWord = function() {
+    return this.instruments[Math.floor(Math.random() * this.instruments.length)];
+  }
+  this.word = new LtrTrans(this.randomWord());
+  // turn letters in word into _
   this.splitBlanks = this.word.makeBlank();
+  // split word
   this.splitWord = this.word.split();
+
   this.lives = 9;
+  this.mainMenu = mainMenu;
   this.totalGuesses = [];
   this.lettersGuessed = [];
-  let that = this;
+
   this.playGame = function() {
-    if (this.word.word !== this.splitBlanks.join("")) {
+    if (this.word.word !== that.splitBlanks.join("")) {
       inquirer.prompt([
         {
           name: "userGuess",
           message: "Guess a letter!!!"
         }])
         .then(function(answer) {
+          // console.log(that.word.word);
+          // console.log(that.splitBlanks.join(""));
           that.totalGuesses.push(answer.userGuess);
           for (i = 0; i < that.totalGuesses.length; i++)
             for (j = 0; j < that.splitBlanks.length; j++) {
@@ -28,18 +39,85 @@ var Word = function() {
             }
           }
           if (that.splitBlanks.indexOf(answer.userGuess) < 0) {
-            console.log("INCORRECT!");
-            that.lives --;
-            that.lettersGuessed.push(answer.userGuess);
+            that.wrongLtr();
           }
-          console.log("Guesses Remaining: " + that.lives);
           console.log(that.splitBlanks.join(" "));
-          console.log("Wrong Guesses: " + that.lettersGuessed);
           that.playGame();
         });
     } else {
-      console.log("YOU WIN!!!");
+      console.log("You Won! ctrl + c to exit");
+      // inquirer.prompt([
+      //   {
+      //     type: "list",
+      //     name: "choice",
+      //     message: "\nYOU WIN!!!",
+      //     choices: [
+      //       'Play Again?',
+      //       'Main Menu\n'
+      //     ]
+      //   }
+      // ])
+      // .then(function(response) {
+      //   if (response.choice === 'Play Again?') {
+      //     // that.playGame();
+      //   } else if (response.choice === 'Main Menu\n') {
+      //     // that.mainMenu;
+      //   }
+      // })
     }
   };
-}
+  this.wrongLtr = function() {
+    console.log("INCORRECT!");
+    that.lives --;
+    that.lettersGuessed.push(that.totalGuesses[that.totalGuesses.length -1]);
+    console.log("Guesses Remaining: " + that.lives);
+    console.log("Wrong Letters Guessed: " + that.lettersGuessed);
+    that.endGame();
+  };
+
+  this.endGame = function() {
+    if (that.lives <= 0) {
+
+      inquirer.prompt([
+        {
+          type: "list",
+          name: "choice",
+          message: "\nSorry, Game Over!",
+          choices: [
+            'Play Again!',
+            'Main Menu\n'
+          ]
+        }
+      ])
+      .then(function(response) {
+        if (response.choice === 'Play Again!') {
+          this.word = new LtrTrans(that.randomWord());
+          that.totalGuesses = [];
+          that.lettersGuessed = [];
+          that.lives = 9;
+          that.splitBlanks = this.word.makeBlank();
+          that.splitWord = this.word.split();
+          // that.newGame();
+          // that.playGame();
+        } else if (response.choice === 'Main Menu\n') {
+          // that.mainMenu.mainMenu;
+          // console.log(that.mainMenu);
+        }
+      });
+    }
+  };
+
+  this.newGame = function() {
+    // that.word = new LtrTrans(that.randomWord);
+    this.word = new LtrTrans(that.randomWord());
+    that.totalGuesses = [];
+    that.lettersGuessed = [];
+    that.lives = 9;
+    that.splitBanks = '';
+    that.splitWord = '';
+    console.log(that.splitBlanks.join(" "));
+    mainMenu();
+  };
+};
+
 module.exports = Word;
